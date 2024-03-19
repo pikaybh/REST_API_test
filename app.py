afc659ba9e2cb95d3f4f1f350e470fb5f3c5d218
B.H Yoo <pikabh@naver.com>
2024-03-19 14:44:25 +0900

    add Flask

---
+++ app.py
@@ -0,0 +1,13 @@
+import Flask
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET', 'POST'])
+def home():
+    data = {"message": "Hello, World!"}
+    return jsonify(data), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
